b06a394a613cd38bfb354a802129779f9ce1e9b0
PetersonNormil <petersonnormil@gmail.com>
2022-01-12 16:09:23 -0500

    Created Logic for the tv
    logic that is based on clicking the tv, the layer should dissapear/reappear
    planning on adding a small time delay so user cannot spam it

--- psychologicalHorrorGame/Assets/Scripts/TVInteractable.cs
+++ psychologicalHorrorGame/Assets/Scripts/TVInteractable.cs
@@ -6,12 +6,35 @@ public class TVInteractable : Interactable
 {
     public Transform spawnpoint;
     public GameObject prefab;
+    public bool tvIsOn = false;
 
+    //TODO add a little bit of delay so user cant spam it 
+    void Start()
+    {
+       
+        prefab = Instantiate(prefab, spawnpoint.position, spawnpoint.rotation);
+        //  prefab.SetActive(false);
+
+       // prefab.SetActive(!prefab.activeSelf);
+        //prefab.SetActive(!prefab.activeSelf);
+    }
 
     public override void OnInteract()
     {
         print("Interacted with " + gameObject.name + " Time to make a layer of white stuff appear");
-        Instantiate(prefab, spawnpoint.position, spawnpoint.rotation);
+        prefab.SetActive(!prefab.activeSelf);
+        if (tvIsOn == false)
+        {
+            Debug.Log("tv is turned on");
+            prefab.SetActive(true);
+            tvIsOn = true;
+        } else
+        {
+            Debug.Log("tv is turned off");
+            tvIsOn =false;
+        }
+
+     
 
     }
     public override void OnFocus()
@@ -24,5 +47,7 @@ public class TVInteractable : Interactable
         print("STOPPED LOOKING AT " + gameObject.name);
        
     }
+
+   
 
 }
