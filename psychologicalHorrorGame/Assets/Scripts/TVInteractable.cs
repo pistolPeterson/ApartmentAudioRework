5a001ac1dfdcaf307cac672c1e2c1a0912c547f1
PetersonNormil <petersonnormil@gmail.com>
2022-01-12 15:31:06 -0500

    Tv static feature
    Created a script that will instantiaate a white cube object prefab in front of TV, to symbolize that the tv is on but not working.
    
    tried to make a remote control button but wasnt really working

--- psychologicalHorrorGame/Assets/Scripts/TVInteractable.cs
+++ psychologicalHorrorGame/Assets/Scripts/TVInteractable.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class TVInteractable : Interactable
 {
+    public Transform spawnpoint;
+    public GameObject prefab;
+
 
     public override void OnInteract()
     {
         print("Interacted with " + gameObject.name + " Time to make a layer of white stuff appear");
-        
+        Instantiate(prefab, spawnpoint.position, spawnpoint.rotation);
 
     }
     public override void OnFocus()
